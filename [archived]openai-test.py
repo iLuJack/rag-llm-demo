a726319beace2701e1347e5ed5b3d2442f61dc9c
iLuJack <jack112liu@gmail.com>
2025-03-14 09:09:44 +0800

    test open ai api, conclusion: useless

---
+++ [archived]openai-test.py
@@ -0,0 +1,20 @@
+# this needs a paid plan, so it's nothing but a nonsense. 
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[{
+        "role": "user",
+        "content": "Write a one-sentence bedtime story about a unicorn."
+    }]
+)
+
+print(completion.choices[0].message.content)
